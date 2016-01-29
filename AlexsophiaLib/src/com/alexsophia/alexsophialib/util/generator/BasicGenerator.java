c89b36f8023da2929356b8937ae3845b5c0a11f4
Alexanderlwz <alexander.lwz@gmail.com>
2016-01-29 13:53:13 +0800

    Add two type of generator.

---
+++ AlexsophiaLib/src/com/alexsophia/alexsophialib/util/generator/BasicGenerator.java
@@ -0,0 +1,41 @@
+/**
+ * CopyRight (C) 1989-2989 <Alexander>
+ * Copy Right Author     : Alexander_LWZ
+ * Package Space         : com.alexsophia.alexsophialib.util.generator
+ * JDK Version Used      : jdk1.7.0_45
+ * Comments              : Automatically create a Generator by given a class
+ * Version               : 1.0.0
+ * Create Date           : 2014.2.5
+ * Modification History  
+ * Sr Date       Why & What is modified
+ */
+
+package com.alexsophia.alexsophialib.util.generator;
+
+
+public class BasicGenerator<T> implements Generator<T> {
+	private Class<T> type;
+
+	public BasicGenerator(Class<T> type) {
+		this.type = type;
+	}
+
+	public T next() {
+		try {
+			return type.newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+	}
+
+	/**
+	 * Produce a Default generator given a type token
+	 * 
+	 * @param type
+	 *            type token
+	 * @return generator
+	 */
+	public static <T> Generator<T> create(Class<T> type) {
+		return new BasicGenerator<T>(type);
+	}
+}
---
+++ AlexsophiaLib/src/com/alexsophia/alexsophialib/util/generator/Generator.java
@@ -0,0 +1,17 @@
+/**
+ * CopyRight (C) 1989-2989 <Alexander>
+ * Copy Right Author     : Alexander_LWZ
+ * Package Space         : com.alexsophia.alexsophialib.util.generator
+ * JDK Version Used      : jdk1.7.0_45
+ * Comments              : The interface for all Generator
+ * Version               : 1.0.0
+ * Create Date           : 2014.2.5
+ * Modification History  
+ * Sr Date       Why & What is modified
+ */
+
+package com.alexsophia.alexsophialib.util.generator;
+
+public interface Generator<T> {
+	T next();
+}
